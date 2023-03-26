965b032a80766f89556bbcbd898439f4cbde7c18
Om Prakash <omofficial70@gmail.com>
2023-03-26 13:42:39 +0530

    Addition

---
+++ First.java
@@ -0,0 +1,11 @@
+
+ class First {
+    public static void main(String [] args)
+     {
+        int num1= 12;
+        int num2= 13;
+        byte num3 = 14;
+       int num = num1 + num2 + num3;
+      System.out.println(num);
+    }   
+}
